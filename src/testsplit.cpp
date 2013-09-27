a2306ebbb337bd742e5eee4cb1654c2afcbf4891
Justin Holdsclaw <jjholdsclaw@gmail.com>
2013-09-26 17:29:17 -0700

    Update testsplit.cpp
    Added some comments to clarify how the sockets would fit into this flow.

--- src/testsplit.cpp
+++ src/testsplit.cpp
@@ -32,8 +32,14 @@ int main(int argc, char* argv[]){
 	{
 		if(fTest.getNextSplit(buf))
 		{
+			// here is where you'd want to shoot off buf in a socket
+			
+			// once the client receives the socket, you would need to pass the data
+			// from the socket (buf) into parseSplit along with a second empty buffer (buf2)
+			// and this function will fill buf2 with the raw file data that needs to be written
 			fTest.parseSplit(buf,buf2,size);
 
+			// you've now got the clean file data chunk to be written into the file
 			if(out_file.is_open()){
 				out_file.write(buf2,size);
 				cout << "Wrote #" << nCount++ << endl;
