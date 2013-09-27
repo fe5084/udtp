d7d2235a3f0eca892bb9afa6c9c50cf2380b9c13
Justin Holdsclaw <jjholdsclaw@gmail.com>
2013-09-26 17:17:14 -0700

    Update file.cpp
    Basic start of class structure.  Currently functional, but very rough around the edges. 
    
    So far, the class can: 
    
    -Split file into data chunks
    -Prepare data chunk for packet track (id, chunksize, etc)
    -Parse received packets and reconstruct original file
    
    Just got the basic functionality working and tested, but need to clean it up.

--- src/file.cpp
+++ src/file.cpp
@@ -1,73 +1,139 @@
+/*
+ * file.cpp
+ *
+ *  Created on: Sep 26, 2013
+ *      Author: jholdsclaw
+ */
 
 #include "file.h"
 
+#ifndef SPLIT_SIZE
+#define SPLIT_SIZE 512
+#endif
 
-int file::getNumberOfPackets(){
-	return 0;
+file::file(void)
+{
+	m_uiMaxChunkSize = SPLIT_SIZE;
+}
+
+file::file(const char* _filename)
+{
+	int i = SPLIT_SIZE;
+	processFile(_filename,SPLIT_SIZE);
+}
+
+file::file(const char* _filename, unsigned int _size)
+{
+	processFile(_filename, _size);
+}
+
+file::~file()
+{
+}
+
+bool file::processFile(const char* _filename, unsigned int _size = SPLIT_SIZE)
+{
+	m_uiMaxChunkSize = _size;
+
+	// clear out any ugly memory in szFileName
+	memset(m_fhHeader.szFileName,0,sizeof(m_fhHeader.szFileName));
+	
+	// open file for reading	
+	pFileReader = new ifstream(_filename, ios::binary);
+
+	// check that file opened and exists
+	if(pFileReader->is_open()){
+		// store filename for transmission
+		memcpy(m_fhHeader.szFileName,_filename,strlen(_filename));
+
+		// seek to end for file size
+		pFileReader->seekg(0, pFileReader->end);
+		m_fhHeader.uiSize = pFileReader->tellg();
+
+		// determine # of chunks needed
+		m_fhHeader.nChunks = (pFileReader->tellg()/m_uiMaxChunkSize);
+		
+		// return pointer to start of file
+		pFileReader->seekg(0, pFileReader->beg);
+
+		// set active chunk to 0
+		m_uiActiveChunk = 0;
+
+	}
+
+	return true;
+}
+
+// gets the next chunk of data and builds a "split" struct with the id, pure data chunk size, and data buffer
+bool file::getNextSplit(char* &dest)
+{
+	static char* _buffer;
+	if(m_uiActiveChunk > 0)
+	{
+		delete chunk.buffer;
+		delete _buffer;
+		chunk.buffer = NULL;
+		_buffer = NULL;
+	}
+
+	if(m_uiActiveChunk <= m_fhHeader.nChunks)
+	{
+		//memcpy(&chunk,0,sizeof(chunk));
+
+		chunk.id = m_uiActiveChunk;
+
+		// determine if this split will be max chunk size or remainder for last packet
+		chunk.uiChunkSize = (m_uiMaxChunkSize>=(pFileReader->end - pFileReader->tellg())?m_uiMaxChunkSize:(pFileReader->end - pFileReader->tellg()));
+	
+		chunk.buffer = new char[chunk.uiChunkSize];
+		pFileReader->read(chunk.buffer, chunk.uiChunkSize);
+
+		// copy all of the elements into a new memory chunk
+		unsigned int packet_size = (sizeof(chunk.id) + sizeof(chunk.uiChunkSize) + chunk.uiChunkSize);
+
+		_buffer = new char[packet_size];
+		memcpy(_buffer,&chunk.id,sizeof(chunk.id));
+		memcpy(&_buffer[sizeof(chunk.id)],&chunk.uiChunkSize,sizeof(chunk.uiChunkSize));
+		memcpy(&_buffer[sizeof(chunk.id)+sizeof(chunk.uiChunkSize)],chunk.buffer,chunk.uiChunkSize);
+
+		dest = _buffer;
+
+		m_uiActiveChunk++;
+		return true;
+	}
+	return false;
+}
+
+// returns the pure data buffer, based on the chunk size of this split
+bool file::parseSplit(const char* source, char* &dest, unsigned int &size)
+{
+	static char* _buffer;
+
+	if(_buffer)
+		delete _buffer;
+
+	unsigned int _id,_chunksize;
+
+	memcpy(&_id,source,sizeof(_id));
+	memcpy(&_chunksize,&source[sizeof(_id)],sizeof(_id));
+
+	size = _chunksize;
+
+	_buffer = new char[_chunksize];
+	memcpy(_buffer,&source[sizeof(_id)+sizeof(_chunksize)],_chunksize);
+
+	dest = _buffer;
+
+	return true;
 }
 
 
+void file::setMaxChunkSize(unsigned int _size)
+{
+	m_uiMaxChunkSize = _size;
+}
 
-
-
-
-
-
-
-
-
-//PROTOTYPE CODE IS HERE
-
-
-//#include <iostream>
-//#include <fstream>
-//#include <iomanip>
-//#include <vector>
-//#include <cstring>
-//#include <string>
-//#define SPLIT_SIZE 548
-//
-//using namespace std;
-//
-//vector<char*> split;
-//vector<int> split_this_shit;
-//
-//int main(int argc, char* argv[]){
-//
-//	ifstream open_file("logo_myDog.png", ios::binary);
-//		int file_size;
-//	char* fucking_file_data = new char[46028];
-//	if(open_file.is_open()){
-//		open_file.seekg(0, open_file.end);
-//		cout << "Successfully opened file." << endl;
-//		cout << open_file.tellg() << endl;
-//		file_size = open_file.tellg();
-//		cout << file_size << endl;
-//		//Let's find how many files there are
-//		int packets = (open_file.tellg()/SPLIT_SIZE);
-//		open_file.seekg(0, open_file.beg);
-//		cout << packets << endl;
-//		for(int i=0; i<=packets; i++){
-//			int packet_size = (SPLIT_SIZE>=(open_file.end - open_file.tellg())?SPLIT_SIZE:(open_file.end - open_file.tellg()));
-//			char* read_data = new char[packet_size];
-//			int put_this_shit = packet_size;
-//			open_file.read(read_data, packet_size);
-//			split.push_back(read_data);
-//			split_this_shit.push_back(put_this_shit);
-//		}
-//		unsigned int offset = 0;
-//		for(int i=0; i<=packets; i++){
-//			memcpy(&fucking_file_data[offset], split[i], split_this_shit[i]);
-//			offset += split_this_shit[i];
-//		}
-//
-//	}
-//
-//	ofstream out_file("finally.png", ofstream::binary);
-//	if(out_file.is_open()){
-//		out_file.write(&fucking_file_data[0],file_size);
-//	}
-//	open_file.close();
-//	out_file.close();
-//	cin.get();
-//}
+unsigned int file::getMaxChunkSize()
+{
+	return m_uiMaxChunkSize;
+}
