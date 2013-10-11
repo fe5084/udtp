1b4cdf45afa34060e3a42170de4ddd347634d548
Kevin Trinh <kevintrinh255@gmail.com>
2013-10-10 18:06:54 -0700

    Update udtp.cpp
    NO FUCKING IDE!!! WILL REVISE LATER, ADDED RELAY THREAD

--- src/udtp.cpp
+++ src/udtp.cpp
@@ -6,6 +6,17 @@
 // Description :
 //============================================================================
 #include "udtp.h"
+#define SPLIT_SIZE 512
+//Acknowledgement IDs FOR TCP
+const char* REQUEST_FILE_ID = "00";
+const char* APPROVAL = "01";
+const char* FINISHED_CLIENT = "04";
+const char* WHINE_REQUEST = "05";
+const char* EVERYTHING_IS_GOOD = "06";
+
+
+//Acknowledgement IDs for UDP
+const char* PACKET = "02";
 
 int UDTP::getSocket(){
 	return m_iSocket;
@@ -37,7 +48,7 @@ int UDTP::startServer(int iPort){
 		m_SAddress.sin_family = AF_INET;
 		m_SAddress.sin_addr.s_addr = INADDR_ANY;
 		m_iSocket = socket(AF_INET, SOCK_DGRAM,0);
-		
+
 		m_iPrioritySocket = socket(AF_INET, SOCK_STREAM,0); // init TCP socket
 		if((bind(m_iPrioritySocket, (struct sockaddr*)&m_SAddress, sizeof(m_SAddress))) < 0){	
 			if((bind(m_iSocket, (struct sockaddr*)&m_SAddress, sizeof(m_SAddress)))<0){
@@ -46,19 +57,21 @@ int UDTP::startServer(int iPort){
 				}
 
 		}
-	
+
 	//Listen on TCP Socket
 		if(listen(m_iPrioritySocket, 0) < 0){
 			perror("listen");
 			return 2;	
 		}
-	
+
 	m_bServer = true;
 	m_bAlive = true;
-	pthread_create(&m_PriorityThread, NULL, &UDTP::PriorityThread, (UDTP*)this);
+	pthread_create(&m_RelayThread, NULL, &UDTP::relayThread, (UDTP*) this);
+	pthread_create(&m_PriorityThread, NULL, &UDTP::priorityThread, (UDTP*)this);
 	pthread_create(&m_MainThread, NULL, &UDTP::processThread, (UDTP*)this);
 	pthread_tryjoin_np(m_MainThread, NULL);
-	pthreaD_tryjoin_np(m_PriorityThread, NULL);
+	pthread_tryjoin_np(m_PriorityThread, NULL);
+	pthread_tryjoin_np(m_RelayThread, NULL);
 
 	return 0;
 
@@ -74,6 +87,7 @@ int UDTP::startServer(int iPort){
 	Return: 0 on successful
 			1 socket has been initialized already!
 			4 could not start mutex
+			5 Could not connect to TCP backbone
 
 */
 int UDTP::startClient(char* chAddress, int iPort){
@@ -89,10 +103,20 @@ int UDTP::startClient(char* chAddress, int iPort){
 		m_SDestination.sin_port = htons(m_iPort);
 		m_SDestination.sin_family = AF_INET;
 		m_iSocket = socket(AF_INET, SOCK_DGRAM, 0);
-		m_bServer = false;
+		m_iPrioritySocket = socket(AF_INET, SOCK_STREAM, 0);
+		if(connect(CProperties->m_iPrioritySocket, (struct sockaddr*)&m_SDestinationAddr) <0){
+			//This allows us to connect to the server, if it cannot, then the socket shall not be started.
+				perror("connect");
+				return 5;
+		}
+				m_bServer = false;
 		m_bAlive = true;
+		pthread_create(&m_RelayThread, NULL, &UDTP::relayThread, (UDTP*) this);
+		pthread_create(&m_PriorityThread, NULL, &UDTP::priorityThread, (UDTP*)this);
 		pthread_create(&m_MainThread, NULL, &UDTP::processThread, (UDTP*)this);
-			pthread_tryjoin_np(m_MainThread, NULL);
+		pthread_tryjoin_np(m_MainThread, NULL);
+		pthread_tryjoin_np(m_PriorityThread, NULL);	
+		pthread_tryjoin_np(m_RelayThread, NULL);
 		return 0;
 	}
 
@@ -115,7 +139,25 @@ int UDTP::close(){
  *
  *	... I need more ram
  */
- 
+void *UDTP::relayThread(void* args){
+	UDTP *CProperties = (UDTP*) args;
+	while(CProperties->m_bAlive){
+		if(CProperties->m_bServer){
+			
+			if(CProperties->m_rgTCPRequests.size() > 0){
+				//This allows UDP to send TCP request messages
+				send(CProperties->m_rgTCPRequests[0].m_iSocket, CProperties->m_rgTCPRequests[0].m_chBuffer, strlen(CProperties->m_rgTCPRequests[0].m_chBuffer), 0);
+				//To allow UDP to send TCP messages, you must create a new struct called TCPSend, in there
+				//you will need to specify the TCP Socket number and also the buffer of which you would like to send.
+				//This allows UDP to send whine requests to the relay thread, then it will handle that shit here.
+			}
+			//Server relay thread
+		}else{
+			//Client Relay thread
+		}
+	}
+	return NULL;
+}
 void *UDTP::priorityThread(void* args){
 	UDTP *CProperties = (UDTP*) args;
 	pollfd iTCPSocket;
@@ -125,6 +167,8 @@ void *UDTP::priorityThread(void* args){
 	CProperties->pPollSockets = &m_rgConnections[0];
 	while(CProperties->m_bAlive){
 		//Reiterate the pointer
+		if(CProperties->m_bServer){
+
 		CProperties->pPollSockets = &CProperties->m_rgConnections[0];
 		CProperties->m_iPollActivity = poll(pPollSockets,m_rgConnections.size(),-1);
 		if(m_iPollActivity<0){
@@ -151,17 +195,52 @@ void *UDTP::priorityThread(void* args){
 				SNewClient.m_iPort = ntohs(SClientAddress.sin_port);
 				SNewClient.m_LastActive = time(NULL);
 				CProperties->m_rgClients.push_back(SNewClient);
-				
-				
+
+
 				//Need to rework on IDE, only worked on GitHub editor
-			}
-					
+				}
+
 		}
-		
-		for(int i=0; i<CProperties->m_rgConnections.size(); i++){
+
+
+			for(int i=0; i<CProperties->m_rgConnections.size(); i++){
+				if(CProperties->m_rgConnections[i].revents & POLLIN){
+					char chBuffer[SPLIT_SIZE];
+					if(read(CProperties->m_rgConnections[i].fd, chBuffer, SPLIT_SIZE)) != 0){
+						//Handle packet reading here then appoint it to rightful owner
+						if(memcmp(buffer, REQUEST_FILE_ID, strlen(REQUEST_FILE_ID)) == 0){
+							//Handle request File ID
+						}
+						if(memcmp(buffer, APPROVAL, strlen(APPROVAL)) == 0){
+							//Handle approval
+						}
+						if(memcmp(buffer, FINISHED_CLIENT, strlen(FINISHED_CLIENT)) == 0){
+							//Handle finished client
+							
+						}
+						if(memcmp(buffer, WHINE_REQUEST, strlen(WHINE_REQUEST)) == 0){
+							//Handle whine request
+							
+						}
+						if(memcmp(buffer, EVERYTHING_IS_GOOD, strlen(EVERYTHING_IS_GOOD)) == 0){
+							//handle everything is good.
+							
+						}
+
+					}else{
+						//Client has disconnected over here! Handle that shit
+					}
+				}
 			//Check through all sockets except 1 because that is server socket	
 		}
 		//Sleep and reduce CPU usage
+		}else{
+			//Client TCP Programming
+			//CProperties->m_iPrioritySocket;
+			//Connect to Server
+		
+					
+		}
 		poll(0,0,100);
 	}
 	return NULL;
@@ -175,28 +254,32 @@ void *UDTP::processThread(void* args){
 
 			//Server processThread programming here
 
-			char buffer[SPLIT_SIZE];
+			char chPeekBuffer[SPLIT_SIZE];
 			struct sockaddr_in SClientAddr;
 
 			memset(&SClientAddr, 0, sizeof(SClientAddr));
 			socklen_t fromlen = sizeof(SClientAddr);
 
-			int iByteCount = recvfrom(CProperties->m_iSocket, buffer, sizeof (buffer), MSG_PEEK, (struct sockaddr*)&SClientAddr, &fromlen);
+			int iByteCount = recvfrom(CProperties->m_iSocket, chPeekBuffer, sizeof (chPeekBuffer), MSG_PEEK, (struct sockaddr*)&SClientAddr, &fromlen);
 				//Increment File ID's
-			CProperties->m_uiTestFileId++;
+
 
 				//Treat it as read
-			recvfrom(CProperties->m_iSocket, buffer, sizeof (buffer), 0, (struct sockaddr*)&SClientAddr, &fromlen);
-
+			if(memcpy(chPeekBuffer,PACKET, strlen(PACKET)) == 0){
+				m_SThreadProperties m_SNewProperties;
+				m_SNewProperties m_pcAccess = CProperties;
+				m_SNewProperties m_uiChunkNumber = 000000; //Insert number read from packet
+				pthread_t open_thread;
+				pthread_create(&open_thread, NULL, &UDTP::openThread, (m_SThreadProperties*)&m_SNewProperties);
+				pthread_tryjoin_np(openthread, NULL);
 			}
+
 
 			//			std::cout << inet_ntoa(SClientAddr.sin_addr) << ntohs(SClientAddr.sin_port) << std::endl;
 			//			m_SThreadProperties m_sNewProperties;
 			//			m_sNewProperties.m_pCAccess = CProperties;
 			//			if(one_time){
-			//				pthread_t test_thread;
-			//				pthread_create(&test_thread, NULL, &UDTP::openThread, (m_SThreadProperties*)&m_sNewProperties);
-			//				pthread_tryjoin_np(test_thread, NULL);
+
 			//				one_time = false;
 			//			}
 
@@ -227,4 +310,3 @@ void* UDTP::openThread(void* args){
 	}
 	return (int*) 0;
 }
-
